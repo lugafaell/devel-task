a75eee2a991ae68f3bd9250948969543c07a21b7
Rafael de Menezes <rafaeldemenezes39@gmail.com>
2024-11-09 02:47:08 -0300

    Update app.module.ts

--- task-manager-backend/src/app.module.ts
+++ task-manager-backend/src/app.module.ts
@@ -8,7 +8,7 @@ import { TasksModule } from './tasks/tasks.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: 5432,
+      port: parseInt(process.env.DB_PORT, 10),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
